a25e1f5cdcfae1f97e8ced3bc74f8ff369ef9000
Tim Kershaw <ctkershaw@gmail.com>
2025-06-16 11:58:25 +0100

    workin os map

---
+++ src/lib/workers/geotiffWorker.js
@@ -0,0 +1,26 @@
+import { fromUrl, fromArrayBuffer, fromBlob  }  from 'geotiff';
+
+self.onmessage = async (event) => {
+
+  const url = event.data.url;
+
+  try {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+
+    // Parse GeoTIFF from ArrayBuffer using geotiff 2.1.3 API
+    const tiff = await fromArrayBuffer(arrayBuffer);
+    const image = await tiff.getImage();
+    const rasterData = await image.readRasters();
+
+    // Example: send back first band pixels and width/height
+    self.postMessage({
+      width: image.getWidth(),
+      height: image.getHeight(),
+      bbox: image.getBoundingBox(),
+      data: rasterData, // first band pixel array
+    });
+  } catch (error) {
+    self.postMessage({ error: error.message });
+  }
+};
