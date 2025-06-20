40b59df35b36d30ca0e99342e28bd1d37464c66f
Mike Lister <mike.lister@communities.gov.uk>
2025-06-20 15:30:38 +0100

    Adding file upload and sortable table

---
+++ src/lib/Button.svelte
@@ -0,0 +1,118 @@
+<script>
+  let {
+    textContent = undefined,
+    buttonType,
+    componentNameProp = undefined,
+    onClickFunction = undefined,
+  } = $props();
+
+  let buttonClass = $derived(
+    buttonType === "default"
+      ? "govuk-button"
+      : buttonType === "secondary"
+        ? "govuk-button govuk-button--secondary"
+        : buttonType === "warning"
+          ? "govuk-button govuk-button--warning"
+          : buttonType === "dark background"
+            ? "govuk-button govuk-button--inverse"
+            : undefined
+  );
+</script>
+
+<div class="p-4">
+  {#if buttonType === "start"}
+    <a
+      href="#"
+      role="button"
+      draggable="false"
+      class="govuk-button govuk-button--start"
+      data-module="govuk-button"
+      onclick={onClickFunction}
+    >
+      {textContent}
+      <svg
+        class="govuk-button__start-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="17.5"
+        height="19"
+        viewBox="0 0 33 40"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z"></path>
+      </svg>
+    </a>
+  {:else if buttonType === "disabled"}
+    <button
+      type="submit"
+      disabled
+      aria-disabled="true"
+      class="govuk-button"
+      data-module="govuk-button"
+      onclick={onClickFunction}
+    >
+      {textContent}
+    </button>
+  {:else if buttonType === "table header"}
+    <button type="button" class="text-header" onclick={onClickFunction}>
+      {textContent}
+      <svg
+        width="22"
+        height="22"
+        focusable="false"
+        aria-hidden="true"
+        role="img"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.1875 9.5L10.9609 3.95703L13.7344 9.5H8.1875Z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M13.7344 12.0781L10.9609 17.6211L8.1875 12.0781H13.7344Z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </button>
+  {:else}
+    <button
+      type="submit"
+      class={buttonClass}
+      data-module="govuk-button"
+      onclick={onClickFunction}
+    >
+      {textContent}
+    </button>
+  {/if}
+</div>
+
+<style>
+  .text-header {
+    display: flex;
+    color: #005ea5;
+  }
+
+  button {
+    -webkit-appearance: button;
+    background-color: transparent;
+    background-image: none;
+    box-sizing: border-box;
+    border-width: 0;
+    border-style: solid;
+    border-color: #e5e7eb;
+    cursor: pointer;
+    text-transform: none;
+    font-family: inherit;
+    font-feature-settings: inherit;
+    font-variation-settings: inherit;
+    font-size: 100%;
+    font-weight: inherit;
+    line-height: inherit;
+    letter-spacing: inherit;
+    color: inherit;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/lib/Table.svelte
@@ -0,0 +1,238 @@
+<script>
+  import Button from "./Button.svelte";
+
+  let {
+    data = undefined,
+    metaData = undefined,
+    caption = undefined,
+    colourScale = undefined,
+    sortState = $bindable({ column: "sortedColumn", order: "ascending" }),
+  } = $props();
+
+  let localCopyOfData = $state([...data]);
+
+  function hasUniqueValues(array, key) {
+    const seen = new Set();
+    for (const obj of array) {
+      if (seen.has(obj[key])) {
+        return false; // Duplicate found
+      }
+      seen.add(obj[key]);
+    }
+    return true; // All values are unique
+  }
+
+  let columns = [];
+
+  for (const column in localCopyOfData[0]) {
+    // create a variable to store whether the key is unique or not
+    const keyIsUnique = hasUniqueValues(localCopyOfData, column);
+    // get data type of each column
+    const columnDataType = typeof localCopyOfData[0][column];
+    // for each one create an object and push it into the array
+    const columnObject = {
+      key: column,
+      isUnique: keyIsUnique,
+      dataType: columnDataType,
+    };
+    columns.push(columnObject);
+  }
+
+  $inspect("columns array is ", columns);
+
+  const metrics = columns
+    .filter((column) => column.dataType === "number")
+    .map((column) => column.key);
+
+  //   let sortState = $state({ column: "sortedColumn", order: "ascending" });
+
+  function updateSortState(columnToSort, sortOrder) {
+    sortState.column = columnToSort;
+    sortState.order = sortOrder;
+  }
+
+  function sortFunction() {
+    if (typeof localCopyOfData[0][sortState["column"]] === "number") {
+      if (sortState.order === "ascending") {
+        localCopyOfData.sort(
+          (a, b) => a[sortState.column] - b[sortState.column]
+        );
+      } else {
+        localCopyOfData.sort(
+          (a, b) => b[sortState.column] - a[sortState.column]
+        );
+      }
+    }
+    if (typeof localCopyOfData[0][sortState["column"]] === "string") {
+      if (sortState.order === "ascending") {
+        localCopyOfData.sort((a, b) =>
+          a[sortState["column"]].localeCompare(b[sortState["column"]])
+        );
+      } else {
+        localCopyOfData.sort((a, b) =>
+          b[sortState["column"]].localeCompare(a[sortState["column"]])
+        );
+      }
+    }
+  }
+  if (sortState.column !== "sortedColumn") {
+    sortFunction();
+  }
+
+  //   $inspect(
+  //     sortState,
+  //     localCopyOfData[0],
+  //     typeof localCopyOfData[0][sortState["column"]]
+  //   );
+
+  // heat map
+
+  // calculate the min and max of each metric
+  const minAndMaxValues = {}; // create an empty object to store them in
+  for (const metric of metrics) {
+    // get the values
+    const metricValues = localCopyOfData.map((item) => item[metric]);
+    const min = Math.min(...metricValues);
+    const max = Math.max(...metricValues);
+    // store them
+    minAndMaxValues[metric] = { min, max };
+  }
+
+  localCopyOfData = localCopyOfData.map((row) => {
+    const rowWithNorms = { ...row };
+
+    for (const metric of metrics) {
+      const { min, max } = minAndMaxValues[metric];
+      const value = row[metric];
+      const normalisedValue = (value - min) / (max - min);
+
+      rowWithNorms[`${metric}__normalised`] = normalisedValue;
+    }
+
+    return rowWithNorms;
+  });
+
+  function normToColor(norm) {
+    const hue = 120 * norm;
+    return `hsl(${hue}, 100%, 80%)`;
+  }
+
+  function normToColorReverse(norm) {
+    const hue = 120 * (1 - norm);
+    return `hsl(${hue}, 100%, 80%)`;
+  }
+
+  const colorKey = Object.entries({ Good: 1, Ok: 0.5, Bad: 0 });
+</script>
+
+<div class="p-4">
+  {#if colourScale === "On"}
+    <div class="legend">
+      <div>Colour key:</div>
+      {#each colorKey as key}
+        <div class="color-keys" style="background-color: {normToColor(key[1])}">
+          {key[0]}
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  <div class="table-container">
+    <div id="table-caption" class="sticky-caption">{caption}</div>
+    <table class="govuk-table" data-module="moj-sortable-table">
+      <thead class="govuk-table__head"
+        ><tr class="govuk-table__row">
+          {#each columns as column}
+            <th
+              scope="col"
+              class={`govuk-table__header ${column.dataType === "number" ? "govuk-table__header--numeric" : ""}`}
+              title={metaData[column.key].explainer}
+            >
+              <div class="header">
+                <Button
+                  textContent={metaData[column.key].shortLabel}
+                  buttonType={"table header"}
+                  onClickFunction={() => {
+                    const newDirection =
+                      sortState.column === column.key &&
+                      sortState.order === "ascending"
+                        ? "descending"
+                        : "ascending";
+
+                    updateSortState(column.key, newDirection);
+                    sortFunction();
+                  }}
+                ></Button>
+              </div></th
+            >
+          {/each}
+        </tr></thead
+      >
+      <tbody class="govuk-table__body">
+        {#each localCopyOfData as row}
+          <tr class="govuk-table__row">
+            {#each columns as column}
+              {#if column.dataType === "number"}
+                {#if colourScale === "On"}
+                  <td
+                    class="govuk-table__cell govuk-table__cell--numeric"
+                    style="background-color: {metaData[column.key].direction ===
+                    'Higher is better'
+                      ? normToColor(row[column.key + '__normalised'])
+                      : normToColorReverse(row[column.key + '__normalised'])}"
+                    >{row[column.key]}</td
+                  >
+                {:else}
+                  <td class="govuk-table__cell govuk-table__cell--numeric"
+                    >{row[column.key].toLocaleString()} ha</td
+                  >
+                {/if}
+              {:else}
+                <td class="govuk-table__cell">{row[column.key]}</td>
+              {/if}
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<style>
+  .table-container {
+    max-height: 85vh;
+    overflow-y: auto;
+    overflow-x: scroll;
+    width: 100%;
+  }
+
+  th {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    background-color: white;
+  }
+
+  .sticky-caption {
+    position: sticky;
+    top: 0;
+    z-index: 2;
+    background-color: white;
+  }
+
+  .legend {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin: 10px;
+  }
+  .color-keys {
+    border-radius: 10%;
+    padding: 6px;
+  }
+
+  .text-header {
+    display: flex;
+    color: #005ea5;
+  }
+</style>
