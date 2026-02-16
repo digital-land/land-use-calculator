bf73f366ec52e761cfa034fd584783490d442518
Tim Kershaw <ctkershaw@gmail.com>
2026-02-16 17:33:05 +0000

    added tileGrid for next step of translating bboxes to tile requests

--- src/routes/testingTiling/+page.svelte
+++ src/routes/testingTiling/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import { onMount } from "svelte";
   import { base } from "$app/paths";
+  import tileGrid from './tiles'
 
+  
   onMount( async () => {
     console.time("tileWorker");
     const tilerWorker = new Worker(
@@ -13,7 +15,9 @@ tilerWorker.onerror = (err) => {
   console.error("Worker error:", err);
 };
 
-    const tileCodes = {
+    const tileCodes = { //QUESTION IS - How do we want to identify the tiles we need? A single bounding box approach maybe? 
+      //Where the ultimate metadata provides the bounds of each feature layer 
+      //Or the user selects their own area which has bounds.
       "8,10": `SUNE`,
       "9,10": `SUNW`,
       "8,11": `SUSW`
